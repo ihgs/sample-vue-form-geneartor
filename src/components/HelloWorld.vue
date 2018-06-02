4d796d70bc59fc832df948167e29444f2d04b40f
ihgs <hgs.dev@gmail.com>
2018-06-02 15:14:44 +0900

    Use bootstrap css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class='panel-body'>
-    <vue-form-generator :schema='schema' :model='model' :options='formOptions'></vue-form-generator>
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading">Form</div>
+      <div class='panel-body'>
+        <vue-form-generator :schema='schema' :model='model' :options='formOptions'></vue-form-generator>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -74,5 +79,26 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
+.panel {
+  margin-bottom: 20px;
+  background-color: #fff;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+  box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+  border-color: #ddd;
+}
+.panel-heading {
+  color: #333;
+  background-color: #f5f5f5;
+  border-color: #ddd;
 
+  padding: 10px 15px;
+  border-bottom: 1px solid transparent;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+}
+.panel-body {
+  padding: 15px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
