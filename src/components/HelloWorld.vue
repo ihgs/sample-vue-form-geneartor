7ef13cde609c5a9913795cfb1011adfd03f0ea78
ihgs <hgs.dev@gmail.com>
2018-06-02 15:00:51 +0900

    Use vue-form-generator

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class='panel-body'>
+    <vue-form-generator :schema='schema' :model='model' :options='formOptions'></vue-form-generator>
   </div>
 </template>
 
 <script>
+import VueFormGenerator from 'vue-form-generator'
+import Vue from 'vue'
+Vue.use(VueFormGenerator)
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      model: {
+        id: 1,
+        name: 'John Doe',
+        password: 'J0hnD03!x4',
+        skills: ['Javascript', 'VueJS'],
+        email: 'john.doe@gmail.com',
+        status: true
+      },
+      schema: {
+        fields: [{
+          type: 'input',
+          inputType: 'text',
+          label: 'ID (disabled text field)',
+          model: 'id',
+          readonly: true,
+          disabled: true
+        }, {
+          type: 'input',
+          inputType: 'text',
+          label: 'Name',
+          model: 'name',
+          placeholder: 'Your name',
+          featured: true,
+          required: true
+        }, {
+          type: 'input',
+          inputType: 'password',
+          label: 'Password',
+          model: 'password',
+          min: 6,
+          required: true,
+          hint: 'Minimum 6 characters',
+          validator: VueFormGenerator.validators.string
+        }, {
+          type: 'select',
+          label: 'Skills',
+          model: 'skills',
+          values: ['Javascript', 'VueJS', 'CSS3', 'HTML5']
+        }, {
+          type: 'input',
+          inputType: 'email',
+          label: 'E-mail',
+          model: 'email',
+          placeholder: 'User\'s e-mail address'
+        }, {
+          type: 'checkbox',
+          label: 'Status',
+          model: 'status',
+          default: true
+        }]
+      },
+
+      formOptions: {
+        validateAfterLoad: true,
+        validateAfterChanged: true
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
